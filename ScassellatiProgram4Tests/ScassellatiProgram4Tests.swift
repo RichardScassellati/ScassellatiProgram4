4815a144cfb64b3c789553b875d4e5a871b00e3d
Richard Scassellati <rn.scassellati@setonhill.edu>
2020-02-26 10:08:38 -0500

    Initial Commit

---
+++ ScassellatiProgram4Tests/ScassellatiProgram4Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ScassellatiProgram4Tests.swift
+//  ScassellatiProgram4Tests
+//
+//  Created by Richard Scassellati on 2/26/20.
+//  Copyright © 2020 Smecko Geck Productions. All rights reserved.
+//
+
+import XCTest
+@testable import ScassellatiProgram4
+
+class ScassellatiProgram4Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
